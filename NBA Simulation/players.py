c625fdc10bc022164147215b30a5ab58bcc9aff5
obaduyi <143231633+obaduyi@users.noreply.github.com>
2026-05-05 14:15:47 -0400

    Add players to Lakers, Celtics, and Warriors. Also, completed the project
    It allows you to pick teams and simulates a winner based on the stats in the Players dict.

--- NBA Simulation/players.py
+++ NBA Simulation/players.py
@@ -1,6 +1,16 @@
 PLAYERS = {
-    "Lakers": [{"name": "LeBron James", "rating": 95}, ...],
-    "Celtics": [{"name": "Jayson Tatum", "rating": 92}, ...],
+    "Lakers": [
+        {"name": "LeBron James", "rating": 95},
+        {"name": "Anthony Davis", "rating": 90}
+    ],
+    "Celtics": [
+        {"name": "Jayson Tatum", "rating": 92},
+        {"name": "Jaylen Brown", "rating": 88}
+    ],
+    "Warriors": [
+        {"name": "Stephen Curry", "rating": 96},
+        {"name": "Klay Thompson", "rating": 87}
+    ]
 }
 
 def get_best_player(team_name):
